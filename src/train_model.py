9156fcd85e0f125f0dd9c429b3e03814978e8227
LorenzoGurrola <lorenzogurrola521@gmail.com>
2024-08-26 14:22:09 -0500

    back prop not working yet

--- src/train_model.py
+++ src/train_model.py
@@ -5,56 +5,50 @@ import torch
 import torch.nn as nn
 
 def load_data():
-    mean = 5 
     std_dev = 1
-    x = np.linspace(mean - 4*std_dev, mean + 4*std_dev, 100)
-    x_train, x_test = train_test_split(x, train_size=0.8)
-    y_train = (x_train>6).astype(int)
-    y_test = (x_test>6).astype(int)
-
-    x_train = np.reshape(x_train, (x_train.shape[0], 1))
+    mean = 5
+    x1 = np.linspace(1, 9, 100)
+    x2 = np.linspace(3, 11, 100)
+    y = ((x1 > 6) & (x2 > 9)).astype(int)
+    X = np.stack((x1, x2), axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=10)
     y_train = np.reshape(y_train, (y_train.shape[0], 1))
-    x_test = np.reshape(x_test, (x_test.shape[0], 1))
     y_test = np.reshape(y_test, (y_test.shape[0], 1))
+    return X_train, X_test, y_train, y_test
 
-    train_data = {'x_train':x_train, 'y_train':y_train}
-    test_data = {'x_test':x_test, 'y_test':y_test}
-    return train_data, test_data
-
-def initialize_params():
-    w = np.random.rand(1,1) * 0.1
-    b = np.zeros((1,1))
-    params = {'w':w,'b':b}
+def initialize_params(X):
+    n = X.shape[1]
+    w = np.random.randn(n, 1) * 0.1
+    b = np.zeros((1, 1))
+    params = {'w':w, 'b':b}
     return params
 
 def sigmoid(z):
     a = 1/(1 + np.exp(-z))
     return a
 
-def forward_prop(x, params):
-    w, b = params['w'], params['b']
-    z = np.matmul(x, w) + b
+def forward_prop(X, params):
+    w = params['w']
+    b = params['b']
+    z = X @ w + b
     inter_vals = {'z':z}
     yhat = sigmoid(z)
     return yhat, inter_vals
 
 def calculate_cost(yhat, y):
     m = y.shape[0]
-    losses = (y * np.log(yhat)) + ((1 - y) * np.log(1 - yhat))
+    losses = y * np.log(yhat) + (1 - y) * np.log(1 - yhat)
     cost = -np.sum(losses, axis=0, keepdims=True)/m
     return cost
 
-def back_prop(x, yhat, y, inter_vals):
+def back_prop(y, yhat, inter_vals, X):
     m = y.shape[0]
     z = inter_vals['z']
     dc_dyhat = (-1/m) * ((y/yhat) - ((1-y)/(1-yhat)))
-    assert dc_dyhat.shape == (80,1)
     dyhat_dz = sigmoid(z) * (1 - sigmoid(z))
-    assert dyhat_dz.shape == (80,1)
-    dz_dc = dyhat_dz * dc_dyhat
-    dw_dc = np.sum(dz_dc * x, axis=0, keepdims=True)
-    db_dc = np.sum(dz_dc, axis=0, keepdims=True)
-    assert dw_dc.shape == (1,1)
-    assert db_dc.shape == (1,1)
-    grads = {'db':db_dc, 'dw':dw_dc}
+    dc_dz = dc_dyhat * dyhat_dz
+    dc_db = np.sum(dc_dz, axis=0, keepdims=True)
+    dz_dw = np.sum(X, axis=1, keepdims=True)
+    dc_dw = dc_dz * dz_dw
+    grads = {'dw':dc_dw, 'db':dc_db}
     return grads
--- tests/test_train_model.py
+++ tests/test_train_model.py
@@ -7,33 +7,42 @@ import torch.nn as nn
 if True:
     import sys
     sys.path.insert(
-        0, 'C:/Users/Lorenzo/Desktop/Workspace/Github/Project-2/src')
+        0, 'C:/Users/Lorenzo/Desktop/Workspace/Github/Project-3/src')
     from train_model import initialize_params, forward_prop, calculate_cost, back_prop, sigmoid
 
 
 class test_forward_and_back_prop(unittest.TestCase):
 
     def test_basic(self):
-        m = 80
-        x = np.random.randn(m, 1)
-        y = np.random.randint(0, 2, (m, 1))
-        params = initialize_params()
-        yhat, inter_vals = forward_prop(x, params)
+        m = 2
+        X = np.array([[0.8, 0.9], [0.3, 0.5]])
+        w = np.array([[0.5], [0.75]])
+        b = np.array([[1.]])
+        y = np.array([[1], [0]])
+        params = {'w':w,'b':b}
+        yhat, inter_vals = forward_prop(X, params)
         cost = calculate_cost(yhat, y)
-        grads = back_prop(x, yhat, y, inter_vals)
+        grads = back_prop(y, yhat, inter_vals, X)
         dw_result = grads['dw']
         db_result = grads['db']
 
-        x = torch.tensor(x)
+        X = torch.tensor(X)
         y = torch.tensor(y)
-        w, b = torch.tensor(params['w'], requires_grad=True), torch.tensor(params['b'], requires_grad=True)
-        z = x @ w + b
-        yhat = 1/(1 + torch.exp(-z))
+        w = torch.tensor(w, requires_grad=True)
+        b = torch.tensor(b, requires_grad=True)
+        z = (X @ w + b).clone().detach().requires_grad_(True)
+        print(f'z {z}')
+        yhat = (1/(1 + torch.exp(-z))).clone().detach().requires_grad_(True)
         losses = (y * torch.log(yhat)) + ((1 - y) * torch.log(1 - yhat))
         cost = -torch.sum(losses, dim=0, keepdims=True)/m
+        print(f'cost torch {cost}')
         cost.backward()
+        print(f'dyhat {yhat.grad.detach()}')
+        print(f'dz_dw {z.grad}')
         dw_expected = w.grad.detach().numpy()
         db_expected = b.grad.detach().numpy()
+        dyhat = yhat.grad
+        
         
         np.testing.assert_allclose(dw_result, dw_expected)
         np.testing.assert_allclose(db_result, db_expected)
